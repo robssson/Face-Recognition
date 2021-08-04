6a3fb765e96f880ef7cd6a54d80e80f06f2a8cc1
Tomasz <tomekradecki@gmail.com>
2021-08-04 17:52:58 +0200

    Update face_rec.py

--- face_rec.py
+++ face_rec.py
@@ -31,4 +31,34 @@ for name in os.listdir(KNOWN_FACES_DIR):
         known_faces.append(encoding)
         known_names.append(name)
 
+ 
+print('Processing unknown faces...')
+for filename in os.listdir(UNKNOWN_FACES_DIR):
+    print(f'Filename {filename}', end='')
+    image = face_recognition.load_image_file(f'{UNKNOWN_FACES_DIR}/{filename}')
+    locations = face_recognition.face_locations(image, model=MODEL)
+    encodings = face_recognition.face_encodings(image, locations)
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+    print(f', found {len(encodings)} face(s)')
+    for face_encoding, face_location in zip(encodings, locations):
+
+        results = face_recognition.compare_faces(known_faces, face_encoding, TOLERANCE)
+        match = None
+        if True in results:  # If at least one is true, get a name of first of found labels
+            match = known_names[results.index(True)]
+            print(f' - {match} from {results}')
+            top_left = (face_location[3], face_location[0])
+            bottom_right = (face_location[1], face_location[2])
+            color = name_to_color(match)
+            cv2.rectangle(image, top_left, bottom_right, color, FRAME_THICKNESS)
+            top_left = (face_location[3], face_location[2])
+            bottom_right = (face_location[1], face_location[2] + 22)
+            cv2.rectangle(image, top_left, bottom_right, color, cv2.FILLED)
+            cv2.putText(image, match, (face_location[3] + 10, face_location[2] + 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (200, 200, 200), FONT_THICKNESS)
+
+    cv2.imshow(filename, image)
+    cv2.waitKey(0)
+    cv2.destroyWindow(filename)
+        
 
